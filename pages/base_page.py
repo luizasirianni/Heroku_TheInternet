06c96789819a356b520df5d5aee80916d431199a
Luiza Sirianni <80859622+luizasirianni@users.noreply.github.com>
2023-04-13 18:12:54 +0100

    criado arquivo base_page

---
+++ pages/base_page.py
@@ -0,0 +1,38 @@
+from selenium.common import TimeoutException, NoSuchElementException
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage():
+
+    def __init__(self, driver):
+        self.driver = driver # este é o selenium
+
+    def _geturl(self, url):
+        self.driver.get(url)
+
+    def _findelement(self, locator):
+        #estrutura genérica pra localizar qualquer elemento
+        return self.driver.find_element(locator['by'], locator['value'])
+
+    def _click(self, locator):
+        self._findelement(locator).click()
+
+    def _write(self, locator, text):
+        self._findelement(locator).send_keys(text)
+
+    def _visualize(self, locator, timeout=0):
+        #função para os asserts
+        #timeout = qual o tempo que vai esperar algo acontecer
+        if timeout > 0:
+            try:
+                wait = WebDriverWait(self.driver, timeout) #espera explicita
+                wait.until(expected_conditions.visibility_of_element_located((locator['by'], locator['value'])))
+            except TimeoutException:
+                return False
+            return True
+        else:
+            try:
+                return self._findelement(locator).is_displayed()
+            except NoSuchElementException:
+                return False
